f6a3c4d79db47898a6089fe476c49284fcae7c78
Brian Jordan <bjj17@duke.edu>
2019-05-02 20:20:00 -0400

    Fixed the hashing password bug so now passwords are encrypted

--- src/Authoring/AuthoringModel/src/BackendExternalAPI/AuthoringBackend.java
+++ src/Authoring/AuthoringModel/src/BackendExternalAPI/AuthoringBackend.java
@@ -44,7 +44,6 @@ public class AuthoringBackend {
     public void saveToXML(Game newGame){
         XStream mySerializer = new XStream(new DomDriver());
         String gameXMLString = mySerializer.toXML(newGame);
-        System.out.println(myAuthoringData.getCurrentUserID());
         GameInfo savingInfo = new GameInfo(newGame.getTitle(), newGame.getThumbnailID(), newGame.getDescription());
         myAuthoringData.saveGame(gameXMLString, savingInfo);
     }
